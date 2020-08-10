f9d330eccca1dfb0c68b3c02620485db99fbe944
D1zzinho <daevusek@gmail.com>
2020-08-11 00:40:14 +0200

    MediaSession frontend

---
+++ src/app/player/mediaSession.js
@@ -0,0 +1,28 @@
+const video = document.getElementById("video")
+
+video.addEventListener('loadedmetadata', () => {
+    if ('mediaSession' in window.navigator) {
+    
+        navigator.mediaSession.metadata = new MediaMetadata({
+          title: document.getElementById("vtitle").innerHTML,
+          artist: "Anonymous",
+          album: "Best album",
+          artwork: [
+            { src: video.getAttribute("poster"), sizes: '96x96',   type: 'image/png' },
+            { src: video.getAttribute("poster"), sizes: '128x128', type: 'image/png' },
+            { src: video.getAttribute("poster"), sizes: '192x192', type: 'image/png' },
+            { src: video.getAttribute("poster"), sizes: '256x256', type: 'image/png' },
+            { src: video.getAttribute("poster"), sizes: '384x384', type: 'image/png' },
+            { src: video.getAttribute("poster"), sizes: '512x512', type: 'image/png' },
+          ]
+        });
+      
+        // navigator.mediaSession.setActionHandler('play', function() {});
+        // navigator.mediaSession.setActionHandler('pause', function() {});
+        navigator.mediaSession.setActionHandler('seekbackward', function() { video.currentTime -= 10; });
+        navigator.mediaSession.setActionHandler('seekforward', function() { video.currentTime += 10; });
+        // navigator.mediaSession.setActionHandler('previoustrack', function() { video.currentTime -= 10;});
+        // navigator.mediaSession.setActionHandler('nexttrack', function() { video.currentTime += 10; });
+    }
+})
+
