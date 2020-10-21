4c68d612630cdfa1c9c7c4e42b67c31bd7f3dcb9
D1zzinho <daevusek@gmail.com>
2020-10-21 16:46:56 +0200

    Unused imports removed

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {ActivatedRoute, Router} from '@angular/router';
 import {AuthService} from '../auth.service';
